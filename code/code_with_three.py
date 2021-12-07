65fe079e9a1a9230cc6ba1617cf1291f818c8d72
Santeri Rauhala <qupu.sr@gmail.com>
2021-12-07 18:27:59 +0200

    Update code_with_three.py

--- code/code_with_three.py
+++ code/code_with_three.py
@@ -11,29 +11,22 @@ import Adafruit_DHT
 def printter():
     while True:
      time.sleep(5)
+     
+     tlon=lon
+     tlat=lat
+     tabove_sea=above_sea
+     taccel=accel
+     tmoti=moti
+     ttem=tem
+     thum=hum
+     ttapp=tapp
+
      os.system('clear')
-     print()
-     print('-----------------------------------------')
-     print()
-     print('Longitude: ',lon)
-     print('Latitude:  ',lat)
-     print('Above mean sea level: ',above_sea)
-     print()
-     print('-----------------------------------------')
-     print()
-     print(accel)
-     print("Motion detected:", moti)
-     print()
-     print('-----------------------------------------')
-     print()
-     print('Temarature:',tem)
-     print('Humidity:  ',hum)
-     print()
-     print('-----------------------------------------')
+     print(f'\n-----------------------------------------\n\nLongitude: {tlon}\nLatitude:  {tlat}\nAbove mean sea level: {tabove_sea}\n\n-----------------------------------------\n\n{taccel}\nMotion detected: {tmoti}\nHit detected:    {ttapp}\n\n-----------------------------------------\n\nTemarature: {ttem}C\nHumidity: {thum}%\n\n-----------------------------------------')
 
 
      with open("log.txt", 'a') as file:
-         file.write(f'{lon};{lat};{above_sea};{accel};{moti};{tem};{hum}\n')
+         file.write(f'{tlon};{tlat};{tabove_sea};{taccel};{tmoti};{ttapp};{ttem};{thum}\n')
 
 
 
@@ -49,22 +42,30 @@ def gps():
         s = serialPort.readline().decode('utf-8')
         if '$GPGGA' in s:
          s=s.replace('\n','').split(',')
-         lon=f'{float(s[2])/100:0.4f} {s[3]}'
-         lat=f'{float(s[4])/100:0.4f} {s[5]}'
-         above_sea=f'{float(s[9]):0.1f} {s[10]}'
+         if s[4]:
+            lon=f'{float(s[2])/100:0.4f} {s[3]}'
+            lat=f'{float(s[4])/100:0.4f} {s[5]}'
+            above_sea=f'{float(s[9]):0.1f} {s[10]}'
+         else:
+            lon='No FIX'
+            lat='No FIX'
+            above_sea='No FIX'
 
 def acc():
     global accel
     global moti
+    global tapp
+
     i2c = board.I2C()
     acce = adafruit_adxl34x.ADXL345(i2c,0x1d)
     acce.enable_freefall_detection(threshold=10, time=25)
     acce.enable_motion_detection(threshold=18)
-    acce.enable_tap_detection(tap_count=2, threshold=30, duration=50)
-
+    acce.enable_tap_detection(tap_count=1, threshold=10, duration=500)
+    
     while True:
         accel=acce.acceleration
         moti=acce.events['motion']
+        tapp=acce.events['tap']
 
 def temp():
     global hum
@@ -87,7 +88,7 @@ moti=False
 
 
 with open("log.txt", 'a') as file:
-         file.write(f'lon;lat;above_sea;accel;moti;tem;hum\n')
+         file.write(f'lon;lat;above_sea;accel;moti;tapp;tem;hum\n')
 
 t1 = threading.Thread(target=printter)
 t2 = threading.Thread(target=gps)
@@ -103,4 +104,7 @@ t1.join()
 t2.join()
 t3.join()
 t4.join()
+
+
+
 
